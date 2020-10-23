864d4b29c971622c9f2076add6677d9735f8b6ed
Nhat Nguyen <nhat.eric.nguyen@gmail.com>
2020-10-24 01:26:26 +0700

    fix auto update countries

--- routes/countries.js
+++ routes/countries.js
@@ -3,23 +3,32 @@ const jwt = require('jsonwebtoken');
 const axios = require('axios');
 const { verifyToken } = require('./token');
 let countriesModel = require('../models/countries.model');
+const ObjectId = require('mongodb').ObjectID;
 
-router.route('/').post((req, res) => {
-  countriesModel
-    .find()
-    .then((countries) => {
-      res.json(countries[0]);
-    })
-    .catch((err) => res.status(400).json('Error: ' + err));
+router.route('/').post(verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403).send('Forbidden');
+    } else {
+      countriesModel
+        .find()
+        .sort({ _id: -1 })
+        .limit(50)
+        .then((countries) => {
+          res.json(countries[0]);
+        })
+        .catch((err) => res.status(400).json('Error: ' + err));
+    }
+  });
 });
 
 router.route('/add').post(async (req, res) => {
   const summaryUri = 'https://api.covid19api.com/summary';
-  let newUser = new countriesModel();
+  let newRecord = new countriesModel();
   await axios
     .get(summaryUri)
     .then((response) => {
-      newUser = new countriesModel({
+      newRecord = new countriesModel({
         Message: response.data.Message,
         Global: response.data.Global,
         Countries: response.data.Countries,
@@ -29,8 +38,9 @@ router.route('/add').post(async (req, res) => {
     .catch((error) => {
       console.log(error);
     });
-  await newUser
-    .update({ _id: '5f8dd1aa78042d2bb4e85ebe' })
+  console.log('newRecord', newRecord);
+  await newRecord
+    .save()
     .then(() => res.status(201).json('Update successfully'))
     .catch((err) => res.status(400).json('Error: ' + err));
 });
